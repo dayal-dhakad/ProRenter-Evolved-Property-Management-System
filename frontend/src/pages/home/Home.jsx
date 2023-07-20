d3ebb5d586839513cad22ea10233751b672b49d4
barakaganai <barakaganai01@gmail.com>
2023-07-20 18:04:24 +0300

    import header and why to the home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBar from "../../components/navBar/NavBar";
+import Header from "../../components/header/Header";
+import Why from "../../components/why/Why";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <Header />
+      <Why />
     </div>
   );
 }
