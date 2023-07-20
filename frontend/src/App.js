30301050441a1cf626b3d61b45c8de14cb36b8f0
barakaganai <barakaganai01@gmail.com>
2023-07-20 17:12:13 +0300

    import react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <div className="App">here we go</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
