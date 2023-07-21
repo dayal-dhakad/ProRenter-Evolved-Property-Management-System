887fff932b117a24f16d346de94d47f0b0083eb7
barakaganai <barakaganai01@gmail.com>
2023-07-21 13:18:59 +0300

    import components into to home

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
 import Why from "../../components/why/Why";
+import About from "../../components/about/About";
+import Testimonial from "../../components/testimonial/Testimonial";
+import News from "../../components/news/News";
+import Footer from "../../components/footer/Footer";
 
 function Home() {
   return (
@@ -9,6 +13,10 @@ function Home() {
       <NavBar />
       <Header />
       <Why />
+      <About />
+      <Testimonial />
+      <News />
+      <Footer />
     </div>
   );
 }
