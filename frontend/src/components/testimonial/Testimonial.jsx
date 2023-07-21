f5d82299703dc2d48b7d19ab80ef4af838054e6f
barakaganai <barakaganai01@gmail.com>
2023-07-21 13:16:18 +0300

    create Testimonial.jsx

---
+++ frontend/src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,51 @@
+import "./Testimonial.scss";
+
+function Testimonial() {
+  return (
+    <div className="testimonial">
+      <div className="tContainer">
+        <h1>What's People Say About Us</h1>
+        <div className="tContainerList">
+          <div className="tContainerListItem">
+            <div className="tListItem">
+              <h1>Baraka Ganai</h1>
+              <span>Dar Es Salaam</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur itaque, ullam quisquam sed assumenda reiciendis,
+              expedita ea et adipisci dolorem soluta exercitationem quod natus.
+              Dolor qui velit accusamus blanditiis? Qui.
+            </p>
+          </div>
+          <div className="tContainerListItem">
+            <div className="tListItem">
+              <h1>Baraka Ganai</h1>
+              <span>Dar Es Salaam</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur itaque, ullam quisquam sed assumenda reiciendis,
+              expedita ea et adipisci dolorem soluta exercitationem quod natus.
+              Dolor qui velit accusamus blanditiis? Qui.
+            </p>
+          </div>
+          <div className="tContainerListItem">
+            <div className="tListItem">
+              <h1>Baraka Ganai</h1>
+              <span>Dar Es Salaam</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur itaque, ullam quisquam sed assumenda reiciendis,
+              expedita ea et adipisci dolorem soluta exercitationem quod natus.
+              Dolor qui velit accusamus blanditiis? Qui.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
