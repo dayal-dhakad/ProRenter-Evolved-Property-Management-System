981ffa16f41f3f77066cc6a2acc0ded215f849b8
barakaganai <barakaganai01@gmail.com>
2023-07-23 07:32:05 +0300

    import services page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Services from "./pages/services/Services";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
       </BrowserRouter>
     </div>
