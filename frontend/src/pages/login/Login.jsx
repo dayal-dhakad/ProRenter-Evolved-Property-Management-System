8530a1f34b212dd57e4ca06fc738bfe8a2965790
barakaganai <barakaganai01@gmail.com>
2023-07-24 17:18:33 +0300

    create login.jsx

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import NavBar from "../../components/navBar/NavBar";
+
+function Login() {
+  return (
+    <>
+      <NavBar />
+      <div className="register">
+        <div className="registerContainer">
+          <div className="left">
+            <img
+              src="https://images.pexels.com/photos/8962339/pexels-photo-8962339.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <h1>Sign In Here.</h1>
+            <form action="">
+              <div className="formLabel">
+                <label htmlFor="">Username</label>
+                <input type="text" placeholder="name" />
+              </div>
+
+              <div className="formLabel">
+                <label htmlFor="">Password</label>
+                <input type="password" placeholder="password" />
+              </div>
+
+              <button>SING IN</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
