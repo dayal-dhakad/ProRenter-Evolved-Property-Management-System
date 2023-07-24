ae9accead6418ad4fac4a58a169222e14067376a
barakaganai <barakaganai01@gmail.com>
2023-07-24 17:18:01 +0300

    create register.jsx

---
+++ frontend/src/pages/register/Register.jsx
@@ -0,0 +1,44 @@
+import NavBar from "../../components/navBar/NavBar";
+import "./Register.scss";
+
+function Register() {
+  return (
+    <>
+      <NavBar />
+      <div className="register">
+        <div className="registerContainer">
+          <div className="left">
+            <img
+              src="https://images.pexels.com/photos/8962339/pexels-photo-8962339.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <h1>Sign Up Here.</h1>
+            <form action="">
+              <div className="formLabel">
+                <label htmlFor="">Username</label>
+                <input type="text" placeholder="name" />
+              </div>
+              <div className="formLabel">
+                <label htmlFor="">Email</label>
+                <input type="email" placeholder="email" />
+              </div>
+              <div className="formLabel">
+                <label htmlFor="">Password</label>
+                <input type="password" placeholder="password" />
+              </div>
+              <div className="formLabel">
+                <label htmlFor="">Phone Number</label>
+                <input type="number" placeholder="phone number" />
+              </div>
+              <button>JOIN</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
