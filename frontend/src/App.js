6d251aff4ad6bd8b5aeefead8038f23b70f4aa8f
barakaganai <barakaganai01@gmail.com>
2023-07-24 17:19:03 +0300

    import pages ot app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Services from "./pages/services/Services";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/signUp" element={<Register />} />
+          <Route path="/signIn" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
