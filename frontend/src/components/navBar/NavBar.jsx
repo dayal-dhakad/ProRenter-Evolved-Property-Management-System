c995bb4bf5a1d3eea90d1fd1562b2662727c86ad
barakaganai <barakaganai01@gmail.com>
2023-07-24 17:17:42 +0300

    add some links

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -6,19 +6,31 @@ function NavBar() {
     <div className="navBar">
       <div className="navBarComponent">
         <div className="left">
-          <h1>RentalSystem</h1>
+          <Link to="/" className="link">
+            <h1>RentalSystem</h1>
+          </Link>
         </div>
         <div className="middle">
-          <span>Home</span>
-          <span>About Us</span>
+          <Link to="/" className="link">
+            <span>Home</span>
+          </Link>
+          <Link to="/" className="link">
+            <span>About Us</span>
+          </Link>
+          <Link to="/" className="link">
+            <span>Contact Us</span>
+          </Link>
           <Link to="/services" className="link">
             <span>Services</span>
           </Link>
-          <span>Contact Us</span>
         </div>
         <div className="right">
-          <span>SIGN IN</span>
-          <span>JOIN</span>
+          <Link to="/signIn" className="link">
+            <span>SIGN IN</span>
+          </Link>
+          <Link to="/signUp" className="link">
+            <span>JOIN</span>
+          </Link>
         </div>
       </div>
     </div>
