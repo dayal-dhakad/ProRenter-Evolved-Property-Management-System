6ff9dd808e4ace1dc8523c67d2192c6ac6638196
barakaganai <barakaganai01@gmail.com>
2023-07-26 15:54:36 +0300

    fetch  data from the db and enable filtering

--- frontend/src/pages/services/Services.jsx
+++ frontend/src/pages/services/Services.jsx
@@ -1,12 +1,51 @@
 import NavBar from "../../components/navBar/NavBar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import axios from "axios";
 import "./Services.scss";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Services() {
   const [show, setShow] = useState(false);
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [location, setLocation] = useState([]);
+
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await axios.get(
+        "http://localhost:5000/api/services" + search
+      );
+      setData(res.data);
+    };
+    getPosts();
+  }, [search]);
+
+  useEffect(() => {
+    const getCategory = async () => {
+      const res = await axios.get("http://localhost:5000/api/categories");
+      setCategory(res.data);
+    };
+    getCategory();
+  }, []);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      const res = await axios.get("http://localhost:5000/api/location");
+      setLocation(res.data);
+    };
+    getLocation();
+  }, []);
+
+  const handleClick = (index) => {
+    if (show === index) {
+      setShow(false);
+    } else {
+      setShow(index);
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -15,252 +54,97 @@ function Services() {
           <div className="left">
             <h1>Search</h1>
             <input type="text" placeholder="Search House, Apartment or Room" />
-            <div className="leftContainer">
-              <h2>Filter By Location</h2>
-              <div className="lContainer" onClick={() => setShow(!show)}>
-                <div className="checkedC">
-                  <input type="checkbox" />
-                  <span>Dar es salaam</span>
+            <h2>Filter By Location</h2>
+            {location.map((itemLocation, index) => (
+              <div className="leftContainer" key={itemLocation._id}>
+                <div className="lContainer" onClick={() => handleClick(index)}>
+                  <div className="checkedC">
+                    <input type="checkbox" />
+                    {itemLocation && (
+                      <Link
+                        to={`/services/?city=${itemLocation.cityName}`}
+                        className="link"
+                      >
+                        <span>{itemLocation.cityName}</span>
+                      </Link>
+                    )}
+                  </div>
                 </div>
-                {show ? (
-                  <FontAwesomeIcon
-                    icon={faChevronUp}
-                    className="checkIcon"
-                    onClick={() => setShow(!show)}
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    icon={faChevronDown}
-                    className="checkIcon"
-                    onClick={() => setShow(!show)}
-                  />
+                {show === index && (
+                  <div className="lContainerItem">
+                    {itemLocation.districtName.map((i) => (
+                      <Link
+                        to={`/services/?district=${i}`}
+                        className="link"
+                        key={i}
+                      >
+                        <span>{i}</span>
+                      </Link>
+                    ))}
+                  </div>
                 )}
               </div>
-              {show && (
-                <div className="lContainerItem">
-                  <span>Kigamboni</span>
-                  <span>Temeke</span>
-                  <span>Ilala</span>
-                  <span>Posta</span>
-                  <span>Ubungo</span>
-                  <span>Mbezi</span>
-                  <span>Kimara</span>
-                </div>
-              )}
-            </div>
+            ))}
           </div>
           <div className="middle">
-            <div className="middleContainer">
-              <div className="imageContainer">
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-              </div>
-              <p>
-                Category: <strong>Self Contained</strong>
-              </p>
-              <span>
-                Number of Rooms: <strong>4</strong>
-              </span>
-              <span>
-                Price: <strong>80,000 TZS per month</strong>
-              </span>
-              <span>
-                Reach me here: <strong>0768 086 183</strong>
-              </span>
-              <span>
-                Status: <strong>Available</strong>
-              </span>
-              <div className="location">
+            {data.map((item) => (
+              <div className="middleContainer" key={item._id}>
+                {item.photo && (
+                  <div className="imageContainer">
+                    {item.photo.map((i) => (
+                      <img src={i} alt="" key={i} />
+                    ))}
+                  </div>
+                )}
+                <p>
+                  Category: <strong>{item.category}</strong>
+                </p>
                 <span>
-                  City: <strong>Dar Es Salaam</strong>
+                  Number of Rooms: <strong>{item.roomNumber}</strong>
                 </span>
                 <span>
-                  District: <strong>kigamboni</strong>
+                  Price: <strong>{item.price} TZS per month</strong>
                 </span>
                 <span>
-                  Street: <strong>Ferry</strong>
+                  Reach me here: <strong>{item.phoneNumber}</strong>
                 </span>
+                <span>
+                  Status: <strong>{item.status}</strong>
+                </span>
+                <div className="location">
+                  <span>
+                    City: <strong>{item.city}</strong>
+                  </span>
+                  <span>
+                    District: <strong>{item.district}</strong>
+                  </span>
+                  <span>
+                    Street: <strong>{item.street}</strong>
+                  </span>
+                </div>
+                <span>
+                  Uploaded By: <strong>James</strong>
+                </span>
+                <span>
+                  Time uploaded:{" "}
+                  <strong>{new Date(item.createdAt).toDateString()}</strong>
+                </span>
+                <button>Book Now</button>
               </div>
-              <span>
-                Uploaded By: <strong>James</strong>
-              </span>
-              <button>Book Now</button>
-            </div>
-            <div className="middleContainer">
-              <div className="imageContainer">
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-              </div>
-              <p>Category: Self Contained</p>
-              <span>Price: 80,000 TZS per month</span>
-              <span>Reach me here: 0768 086 183</span>
-              <span>Status: Available</span>
-              <div className="location">
-                <span>City: Dar Es Salaam</span>
-                <span>District: kigamboni</span>
-                <span>Street: Ferry</span>
-              </div>
-              <button>Book House</button>
-            </div>
-            <div className="middleContainer">
-              <div className="imageContainer">
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-              </div>
-              <p>Category: Self Contained</p>
-              <span>Price: 80,000 TZS per month</span>
-              <span>Reach me here: 0768 086 183</span>
-              <span>Status: Available</span>
-              <div className="location">
-                <span>City: Dar Es Salaam</span>
-                <span>District: kigamboni</span>
-                <span>Street: Ferry</span>
-              </div>
-              <button>Book House</button>
-            </div>
-            <div className="middleContainer">
-              <div className="imageContainer">
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-              </div>
-              <p>Category: Self Contained</p>
-              <span>Price: 80,000 TZS per month</span>
-              <span>Reach me here: 0768 086 183</span>
-              <span>Status: Available</span>
-              <div className="location">
-                <span>City: Dar Es Salaam</span>
-                <span>District: kigamboni</span>
-                <span>Street: Ferry</span>
-              </div>
-              <button>Book House</button>
-            </div>
-            <div className="middleContainer">
-              <div className="imageContainer">
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-              </div>
-              <p>Category: Self Contained</p>
-              <span>Price: 80,000 TZS per month</span>
-              <span>Reach me here: 0768 086 183</span>
-              <span>Status: Available</span>
-              <div className="location">
-                <span>City: Dar Es Salaam</span>
-                <span>District: kigamboni</span>
-                <span>Street: Ferry</span>
-              </div>
-              <button>Book House</button>
-            </div>
-            <div className="middleContainer">
-              <div className="imageContainer">
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-                <img
-                  src="https://images.pexels.com/photos/17109095/pexels-photo-17109095/free-photo-of-pufas-surrounding-small-table-and-tree-pot-in-lounge-area.jpeg?auto=compress&cs=tinysrgb&w=800"
-                  alt=""
-                />
-              </div>
-              <p>Category: Self Contained</p>
-              <span>Price: 80,000 TZS per month</span>
-              <span>Reach me here: 0768 086 183</span>
-              <span>Status: Available</span>
-              <div className="location">
-                <span>City: Dar Es Salaam</span>
-                <span>District: kigamboni</span>
-                <span>Street: Ferry</span>
-              </div>
-              <button>Book House</button>
-            </div>
+            ))}
           </div>
           <div className="right">
             <h1>Categories</h1>
             <h2>Filter By Category</h2>
-            <div className="rightContainer">
-              <span>Full House</span>
-              <span>Apartment</span>
-              <span>Self Contained</span>
-              <span>Double Room</span>
-              <span>Single Room</span>
-            </div>
+            {category.map((itemCategory) => (
+              <div className="rightContainer" key={itemCategory._id}>
+                {itemCategory.name.map((i) => (
+                  <Link to={`/services/?cat=${i}`} className="link" key={i}>
+                    <span key={i}>{i}</span>
+                  </Link>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
