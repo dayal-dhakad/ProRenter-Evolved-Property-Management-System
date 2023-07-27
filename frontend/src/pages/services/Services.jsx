e225fe3a72cf7d1035e2d524fbd1ae1c9b5a1cb0
barakaganai <barakaganai01@gmail.com>
2023-07-27 18:15:15 +0300

    add search functionality

--- frontend/src/pages/services/Services.jsx
+++ frontend/src/pages/services/Services.jsx
@@ -9,6 +9,7 @@ function Services() {
   const [data, setData] = useState([]);
   const [category, setCategory] = useState([]);
   const [location, setLocation] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { search } = useLocation();
 
@@ -46,6 +47,18 @@ function Services() {
     }
   };
 
+  //handle changes
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Handle search form submission
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Update the URL with the search query parameter
+    window.location.search = `?search=${encodeURIComponent(searchQuery)}`;
+  };
+
   return (
     <>
       <NavBar />
@@ -53,7 +66,17 @@ function Services() {
         <div className="servicesContainer">
           <div className="left">
             <h1>Search</h1>
-            <input type="text" placeholder="Search House, Apartment or Room" />
+            <form onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                placeholder="Search House, Apartment or Room"
+                value={searchQuery}
+                onChange={handleSearchInputChange}
+              />
+              <button type="submit">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </form>
             <h2>Filter By Location</h2>
             {location.map((itemLocation, index) => (
               <div className="leftContainer" key={itemLocation._id}>
@@ -86,50 +109,79 @@ function Services() {
               </div>
             ))}
           </div>
-          <div className="middle">
+          <div className="mid">
             {data.map((item) => (
-              <div className="middleContainer" key={item._id}>
-                {item.photo && (
-                  <div className="imageContainer">
-                    {item.photo.map((i) => (
-                      <img src={i} alt="" key={i} />
-                    ))}
+              <div className="middle">
+                <Link
+                  to={`/services/${item._id}`}
+                  key={item._id}
+                  className="link"
+                >
+                  <div className="middleContainer" key={item._id}>
+                    {item.photo && (
+                      <div className="imageContainer">
+                        {item.photo.map((i) => (
+                          <img src={i} alt="" key={i} />
+                        ))}
+                      </div>
+                    )}
+
+                    <p>
+                      Category:{" "}
+                      <strong style={{ color: "darkBlue" }}>
+                        {item.category}
+                      </strong>
+                    </p>
+                    <span>
+                      Number of Rooms:{" "}
+                      <strong style={{ color: "purple" }}>
+                        {item.roomNumber}
+                      </strong>
+                    </span>
+                    <span>
+                      Price:{" "}
+                      <strong style={{ color: "darkGreen" }}>
+                        {item.price} TZS per month
+                      </strong>
+                    </span>
+                    <span>
+                      Reach me here: <strong>{item.phoneNumber}</strong>
+                    </span>
+                    <span>
+                      Status:{" "}
+                      <strong style={{ color: "green" }}>{item.status}</strong>
+                    </span>
+                    <div className="location">
+                      <span>
+                        City:{" "}
+                        <strong style={{ color: "brown" }}>{item.city}</strong>
+                      </span>
+                      <span>
+                        District:{" "}
+                        <strong style={{ color: "brown" }}>
+                          {item.district}
+                        </strong>
+                      </span>
+                      <span>
+                        Street:{" "}
+                        <strong style={{ color: "brown" }}>
+                          {item.street}
+                        </strong>
+                      </span>
+                    </div>
+                    <span>
+                      Uploaded By:{" "}
+                      <strong style={{ color: "Teal" }}>James</strong>
+                    </span>
+                    <span>
+                      Time uploaded:{" "}
+                      <strong style={{ color: "gray" }}>
+                        {new Date(item.createdAt).toDateString()}
+                      </strong>
+                    </span>
+                    <button>View Description</button>
                   </div>
-                )}
-                <p>
-                  Category: <strong>{item.category}</strong>
-                </p>
-                <span>
-                  Number of Rooms: <strong>{item.roomNumber}</strong>
-                </span>
-                <span>
-                  Price: <strong>{item.price} TZS per month</strong>
-                </span>
-                <span>
-                  Reach me here: <strong>{item.phoneNumber}</strong>
-                </span>
-                <span>
-                  Status: <strong>{item.status}</strong>
-                </span>
-                <div className="location">
-                  <span>
-                    City: <strong>{item.city}</strong>
-                  </span>
-                  <span>
-                    District: <strong>{item.district}</strong>
-                  </span>
-                  <span>
-                    Street: <strong>{item.street}</strong>
-                  </span>
-                </div>
-                <span>
-                  Uploaded By: <strong>James</strong>
-                </span>
-                <span>
-                  Time uploaded:{" "}
-                  <strong>{new Date(item.createdAt).toDateString()}</strong>
-                </span>
-                <button>Book Now</button>
+                </Link>
               </div>
             ))}
           </div>
