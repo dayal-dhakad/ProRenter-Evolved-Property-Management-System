f907de6ac0662a398da0a14a70b6596a3137a8e7
barakaganai <barakaganai01@gmail.com>
2023-08-07 13:15:33 +0300

    implement register

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,8 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../../components/navBar/NavBar";
 import "./Register.scss";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 
 function Register() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const { loading, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "REGISTER_START" });
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        credentials
+      );
+      dispatch({ type: "REGISTER_SUCCESS", payload: res.data });
+      navigate("/signIn");
+    } catch (error) {
+      dispatch({ type: "REGISTER_FAILURE", payload: error.message });
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -19,21 +53,34 @@ function Register() {
             <form action="">
               <div className="formLabel">
                 <label htmlFor="">Username</label>
-                <input type="text" placeholder="name" />
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="name"
+                  onChange={handleChange}
+                />
               </div>
               <div className="formLabel">
                 <label htmlFor="">Email</label>
-                <input type="email" placeholder="email" />
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="email"
+                  onChange={handleChange}
+                />
               </div>
               <div className="formLabel">
                 <label htmlFor="">Password</label>
-                <input type="password" placeholder="password" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="password"
+                  onChange={handleChange}
+                />
               </div>
-              <div className="formLabel">
-                <label htmlFor="">Phone Number</label>
-                <input type="number" placeholder="phone number" />
-              </div>
-              <button>JOIN</button>
+              <button disabled={loading} onClick={handleRegister}>
+                JOIN
+              </button>
               <p>
                 already have an account? <Link to="/signIn">sing in here</Link>
               </p>
