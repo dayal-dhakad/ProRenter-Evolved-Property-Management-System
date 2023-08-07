375303a508198ff974673f439b458a5def22bf10
barakaganai <barakaganai01@gmail.com>
2023-08-07 13:17:43 +0300

    implement navBar.js and update

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function NavBar() {
+  const [show, setShow] = useState(false);
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
   return (
     <div className="navBar">
       <div className="navBarComponent">
@@ -28,12 +37,70 @@ function NavBar() {
           </Link>
         </div>
         <div className="right">
-          <Link to="/signIn" className="link">
-            <span>SIGN IN</span>
-          </Link>
-          <Link to="/signUp" className="link">
-            <span>JOIN</span>
-          </Link>
+          {user ? (
+            <>
+              <div className="userImg">
+                <img
+                  src="https://images.pexels.com/photos/8978566/pexels-photo-8978566.jpeg?auto=compress&cs=tinysrgb&w=800"
+                  alt=""
+                  onClick={() => setShow(!show)}
+                />
+              </div>
+              <div className="user">
+                <p onClick={() => setShow(!show)}>{user.username}</p>
+
+                {show ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                    onClick={() => setShow(!show)}
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                    onClick={() => setShow(!show)}
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                )}
+              </div>
+              {show && (
+                <div className="dropMenu">
+                  <p>Settings</p>
+                  <hr />
+                  <p onClick={handleLogout}>Logout</p>
+                </div>
+              )}
+            </>
+          ) : (
+            <>
+              <Link to="/signIn" className="link">
+                <span>SIGN IN</span>
+              </Link>
+              <Link to="/signUp" className="link">
+                <span>JOIN</span>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
