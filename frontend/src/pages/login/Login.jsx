3e17cafdbabf93dfed228f288997907b34718897
barakaganai <barakaganai01@gmail.com>
2023-08-07 13:15:48 +0300

    implement login

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import NavBar from "../../components/navBar/NavBar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 
 function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    dispatch({ type: "LOGIN_START" });
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        credentials
+      );
+
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data.message });
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -19,13 +52,26 @@ function Login() {
             <form action="">
               <div className="formLabel">
                 <label htmlFor="">Username</label>
-                <input type="text" placeholder="name" />
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="name"
+                  onChange={handleChange}
+                />
               </div>
               <div className="formLabel">
                 <label htmlFor="">Password</label>
-                <input type="password" placeholder="password" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="password"
+                  onChange={handleChange}
+                />
               </div>
-              <button>SING IN</button>
+              <button disabled={loading} onClick={handleLogin}>
+                SING IN
+              </button>
+              {error && <span style={{ color: "red" }}>{error}</span>}
               <p>
                 You don't have an account?{" "}
                 <Link to="/signUp">sing up here</Link>
