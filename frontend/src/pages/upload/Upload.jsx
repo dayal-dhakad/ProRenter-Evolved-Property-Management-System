a9985dd4cb4fea411b04c92eb89b6d2f6689091c
barakaganai <barakaganai01@gmail.com>
2023-08-14 16:25:35 +0300

    create upload.jsx

--- frontend/src/pages/upload/Upload.jsx
+++ frontend/src/pages/upload/Upload.jsx
@@ -1,7 +1,79 @@
 import "./Upload.scss";
 import NavBar from "../../components/navBar/NavBar";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Upload() {
+  const [category, setCategory] = useState("");
+  const [roomNumber, setRoomNumber] = useState("");
+  const [price, setPrice] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [status, setStatus] = useState("");
+  const [city, setCity] = useState("");
+  const [district, setDistrict] = useState("");
+  const [street, setStreet] = useState("");
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleFileChange = (e) => {
+    const fileList = e.target.files[0];
+    const fileArray = Array.from(fileList); // Convert FileList to array
+    setSelectedFiles(fileArray);
+  };
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      username: user.username,
+      category,
+      roomNumber,
+      price,
+      phoneNumber,
+      status,
+      city,
+      district,
+      street,
+    };
+
+    try {
+      const formDataArray = [];
+
+      // Create an array of FormData objects for each file
+      for (const file of selectedFiles) {
+        const formData = new FormData();
+        const filename = Date.now() + file.name;
+        formData.append("name", filename);
+        formData.append("file", file);
+        formDataArray.push(formData);
+      }
+
+      // Send requests for each FormData object
+      const uploadPromises = formDataArray.map((formData) =>
+        axios.post("http://localhost:5000/api/upload", formData)
+      );
+
+      // Wait for all requests to complete
+      await Promise.all(uploadPromises);
+
+      console.log("All files have been uploaded");
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      await axios.post("http://localhost:5000/api/services", newPost);
+      navigate("/services");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -9,59 +81,92 @@ function Upload() {
         <div className="uploadContainer">
           <h1>Upload your House, Apartment or Room Here</h1>
           <div className="imageFolder">
-            <img
-              src="https://images.pexels.com/photos/8962339/pexels-photo-8962339.jpeg?auto=compress&cs=tinysrgb&w=800"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/8962339/pexels-photo-8962339.jpeg?auto=compress&cs=tinysrgb&w=800"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/8962339/pexels-photo-8962339.jpeg?auto=compress&cs=tinysrgb&w=800"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/8962339/pexels-photo-8962339.jpeg?auto=compress&cs=tinysrgb&w=800"
-              alt=""
-            />
+            {/* display selected images */}
+            {selectedFiles.map((file, index) => (
+              <img key={index} src={URL.createObjectURL(file)} alt="" />
+            ))}
           </div>
-          <form action="">
+          <form onSubmit={handleUpload} encType="multipart/form-data">
             <div className="label">
-              <label htmlFor="">Category</label>
-              <input type="file" placeholder="Full house, apartment or room" />
+              <label htmlFor="">Upload photos</label>
+              <input
+                type="file"
+                multiple
+                onChange={handleFileChange}
+                required
+              />
             </div>
             <div className="label">
               <label htmlFor="">Category</label>
-              <input type="text" placeholder="Full house, apartment or room" />
+              <input
+                type="text"
+                placeholder="Full house, apartment or room"
+                required
+                onChange={(e) => setCategory(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">Number of Rooms</label>
-              <input type="number" placeholder="Number of Rooms" />
+              <input
+                type="text"
+                placeholder="Number of Rooms"
+                required
+                onChange={(e) => setRoomNumber(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">price</label>
-              <input type="number" placeholder="Price" />
+              <input
+                type="text"
+                placeholder="Price"
+                required
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">Reach Me Here</label>
-              <input type="number" placeholder="Your Phone Number" />
+              <input
+                type="text"
+                placeholder="Your Phone Number"
+                required
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">Status</label>
-              <input type="text" placeholder="Available or Unavailable" />
+              <input
+                type="text"
+                placeholder="Available or Unavailable"
+                required
+                onChange={(e) => setStatus(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">City</label>
-              <input type="text" placeholder="City" />
+              <input
+                type="text"
+                placeholder="City"
+                required
+                onChange={(e) => setCity(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">District</label>
-              <input type="text" placeholder="District" />
+              <input
+                type="text"
+                placeholder="District"
+                required
+                onChange={(e) => setDistrict(e.target.value)}
+              />
             </div>
             <div className="label">
               <label htmlFor="">Street</label>
-              <input type="text" placeholder="Street" />
+              <input
+                type="text"
+                placeholder="Street"
+                required
+                onChange={(e) => setStreet(e.target.value)}
+              />
             </div>
             <button>Upload</button>
           </form>
